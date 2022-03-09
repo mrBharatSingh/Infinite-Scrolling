f7c0c59abd55897d1dc33aae0f01243825ef6eb9
mrBharatSingh <93440682+mrBharatSingh@users.noreply.github.com>
2022-03-09 16:48:00 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const[data,setData]=useState([])
+
+  const makeData=()=>{
+    var arr=[]
+    for(var i=0; i<25; i++)
+    {
+      arr.push("Masai Student")
+    }
+    // console.log(arr)
+    setData((oldData)=>[...oldData,...arr])
+  }
+
+  useEffect(()=>{
+    makeData()
+    var xx=document.getElementById("container")
+    xx.addEventListener('scroll',(e)=>{
+      var a=xx.scrollHeight
+      var b=xx.scrollTop
+      var c=xx.clientHeight
+      if(Math.floor(a-b)<=c+10)
+      {
+        
+        
+        sxrollData()
+      }
+      // console.log(Math.floor(a-b))
+      console.log(xx.clientHeight,"clien")
+    })
+  },[])
+ 
+  const sxrollData=()=>{
+    makeData()
+  }
   return (
     <div className="App">
-      
+      <div id="container">
+     { data.map((el,i)=>{
+        return(<><h1>{el} {i+1}</h1></>)
+      })}
+     
+      </div>
     </div>
   );
 }
